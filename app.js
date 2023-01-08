bf3b567fdfa9a1f9413e422e0f516849a5665974
Alvin Toch <Alvinstoch@live.com>
2023-01-08 00:22:37 -0800

    site has full functionality, backend does not need anymore edits, it appears

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cors = require('cors')
 // const logger = require('morgan')
 
 const AppRouter = require('./routes/AppRouter')
+const { request } = require('http')
+const { response } = require('express')
 
 const PORT = process.env.Port || 8000
 
@@ -14,5 +16,24 @@ app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req,res) => res.json({message: 'Server works'}))
 app.use('/api', AppRouter)
+
+//proxy
+
+// app.use((req, res, next)=>{
+//     res.header('Access-Control-Allow-Origin', '*');
+//     next();
+// })
+// app.get('/v1/cards', (req,res)=>{
+//     request(
+//         { url:`https://api.magicthegathering.io/v1/cards`},
+//         (error, response, body)=>{
+//             if (error|| response.statusCode!==200){
+//                 return res.status(500).json({type:'error', message: err.message});
+//             }
+
+//             res.json(JSON.parse(body));
+//         }
+//     )
+// })
 
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
